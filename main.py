202ade56847bf1216ffb17b902f7c202c2509707
Tamizh iniyan g <trex49001@gmail.com>
2023-02-01 09:20:42 +0530

    retriving iss loc

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import requests
+import datetime as dt
 
 LAT = 11.504776
 LONG = 77.238396
 
 parameter = {
     "lat":LAT,
-    "lng":LONG
+    "lng":LONG,
+    "formatted":0
 }
 
 
+sun_response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameter)
+data = sun_response.json()
 
-response = requests.get(url="https://api.sunrise-sunset.org/json",params=parameter)
-print(response.json())
+iss_response = requests.get(url="http://api.open-notify.org/iss-now.json")
+iss_data = iss_response.json()
+
+
+sunrise = data["results"]['sunrise'].split("T")
+sunset = data["results"]["sunset"].split("T")
+
+
+now  = dt.datetime.now()
+print(now.time())
+
